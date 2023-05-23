c46f13abd38b427e125b0da212b693650324fb27
dxxbjl <13283871702@163.com>
2023-05-23 20:29:31 +0800

    commit login

--- src/main/java/com/dxxbjl/community/controller/LoginController.java
+++ src/main/java/com/dxxbjl/community/controller/LoginController.java
@@ -58,6 +58,7 @@ public class LoginController implements CommunityConstant {
         return "/site/login";
     }
 
+
     @RequestMapping(path = "/register",method = RequestMethod.POST)
     public String register(Model model , User user){
         Map<String,Object> map = userService.register(user);
