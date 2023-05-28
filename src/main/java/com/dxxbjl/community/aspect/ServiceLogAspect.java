f85e799eccdd9f68e395b7f078e677022068ff15
dxxbjl <13283871702@163.com>
2023-05-28 14:45:32 +0800

    commit Spring AOP

---
+++ src/main/java/com/dxxbjl/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,40 @@
+package com.dxxbjl.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    //所有的业务组件、方法都要处理
+    @Pointcut("execution(* com.dxxbjl.community.service.*.*(..))")
+    public void pointcut(){//切点
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint){
+        //用户【1，2，3，4】在【xxxx】，访问了【com.dxxbjl.community.service.xxx】
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].",ip,now,target));
+    }
+}
