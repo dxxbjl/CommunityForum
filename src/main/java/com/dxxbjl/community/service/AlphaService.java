2e7d8d9d05aea64860e9728e272202cbad2980ce
dxxbjl <13283871702@163.com>
2023-06-03 18:11:10 +0800

    commit  Quartz

--- src/main/java/com/dxxbjl/community/service/AlphaService.java
+++ src/main/java/com/dxxbjl/community/service/AlphaService.java
@@ -6,7 +6,11 @@ import com.dxxbjl.community.dao.UserMapper;
 import com.dxxbjl.community.entity.DiscussPost;
 import com.dxxbjl.community.entity.User;
 import com.dxxbjl.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -23,6 +27,7 @@ import java.util.Date;
  */
 @Service
 public class AlphaService {
+    private static final Logger logger = LoggerFactory.getLogger(AlphaService.class);
     @Autowired
     private AlphaDao alphaDao;
 
@@ -107,4 +112,15 @@ public class AlphaService {
             }
         });
     }
+
+    //让该方法在多线程环境下，被异步的调用
+   @Async
+   public void executel(){
+        logger.debug("execute1");
+   }
+
+   @Scheduled(initialDelay = 10000,fixedRate =  10000)
+   public void execute2(){
+       logger.debug("execute2");
+   }
 }
