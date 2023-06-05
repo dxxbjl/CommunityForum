a96396f36146dd5425711baae6cd9880ab19c063
dxxbjl <13283871702@163.com>
2023-06-05 19:48:44 +0800

    commit  caffeine

---
+++ src/test/java/com/dxxbjl/community/CaffeineTest.java
@@ -0,0 +1,42 @@
+package com.dxxbjl.community;
+
+import com.dxxbjl.community.entity.DiscussPost;
+import com.dxxbjl.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTest {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Test
+    public void initDataForTest(){
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost post = new DiscussPost();
+            post.setUserID(111);
+            post.setTitle("狐狸网");
+            post.setContent("狐狸狐狸。。。。。。。。。。");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random()*2000);
+            discussPostService.addDiscussPost(post);
+        }
+    }
+
+    @Test
+    public void testCache(){
+        System.out.println(discussPostService.findDiscussPosts(0,0,10,1));
+        System.out.println(discussPostService.findDiscussPosts(0,0,10,1));
+        System.out.println(discussPostService.findDiscussPosts(0,0,10,1));
+        System.out.println(discussPostService.findDiscussPosts(0,0,10,0));
+    }
+}
